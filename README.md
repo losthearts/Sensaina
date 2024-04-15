76b778d133b6e9a983a0d17e2d512959a4014f95
lost 失われた心 <losthearts@duck.com>
2024-04-15 20:26:00 +0530

    Sensaina:: Added GUI

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Sensaina 繊細な
 
-A terminal tool made to convert sensitivities between games.
+A tool made to convert sensitivities between games.
+
+- Use `sensanai` for the tui application.
+- Use `sensanai -gui` for the gui application.
--- sensaina.go
+++ sensaina.go
@@ -2,17 +2,36 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
+
 	"github.com/charmbracelet/lipgloss"
 	"github.com/charmbracelet/lipgloss/table"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
 )
 
-func main() {
+var initgame string
+var finalgame string
+var initsens float64
+var initdpi int64
+var finaldpi int64
 
-	var initgame string
-	var finalgame string
-	var initsens float64
-	var initdpi int32
-	var finaldpi int32
+func main() {
+	if len(os.Args) != 1 {
+		if os.Args[1] == "-gui" {
+			gui()
+		} else {
+			fmt.Printf("Unexpected argument :: %v\n", os.Args[1])
+		}
+	} else {
+		tui()
+	}
+}
+
+func tui() {
 
 	b := lipgloss.NewStyle().Background(lipgloss.Color("#000")).Bold(true).Italic(true).Underline(true).Foreground(lipgloss.Color("#FDB0C0")).Render
 	v := lipgloss.NewStyle().Foreground(lipgloss.Color("#FDB0C0")).Render
@@ -41,11 +60,61 @@ func main() {
 	fmt.Println(t.Render())
 }
 
-func cmpi(iyaw float64, sensitivity float64, dpi int32) float64 {
+func gui() {
+	sensanaiApp := app.New()
+	formWindow := sensanaiApp.NewWindow("Sensaina 繊細な")
+	formWindow.Resize(fyne.NewSize(600, 500))
+	resultWindow := sensanaiApp.NewWindow("Conversion")
+	resultWindow.Resize(fyne.NewSize(600, 500))
+
+	initgame := widget.NewEntry()
+	initsens := widget.NewEntry()
+	initDPI := widget.NewEntry()
+
+	finalgame := widget.NewEntry()
+	finalDPI := widget.NewEntry()
+
+	form := &widget.Form{
+		Items: []*widget.FormItem{
+			{Text: "The game you want to convert from: ", Widget: initgame},
+			{Text: "Sensitivity in it: ", Widget: initsens},
+			{Text: "DPI in it: ", Widget: initDPI},
+			{Text: "The game you want to convert to: ", Widget: finalgame},
+			{Text: "The DPI you want to convert to: ", Widget: finalDPI}},
+		OnSubmit: func() {
+			fmt.Println("Initial Game:", initgame.Text)
+			fmt.Println("Initial Sensitivity:", initsens.Text)
+			fmt.Println("Initial DPI:", initDPI.Text)
+			fmt.Println("Final Game:", finalgame.Text)
+			fmt.Println("Final DPI:", finalDPI.Text)
+
+			gameI := initgame.Text
+			gameF := finalgame.Text
+			sensI, err := strconv.ParseFloat(initsens.Text, 64)
+			DPIin, err := strconv.ParseInt(initDPI.Text, 10, 32)
+			DPIou, err := strconv.ParseInt(finalDPI.Text, 10, 32)
+
+			fmt.Println(err)
+
+			sensitivity := sens(yaw[gameI], yaw[gameF], sensI, DPIin, DPIou)
+			cm360 := cmpi(yaw[gameI], sensI, DPIin)
+
+			resultWindow.SetContent(widget.NewLabel("Sensitivity:: " + strconv.FormatFloat(sensitivity, 'g', -1, 64) + "\n360/cm ::" + strconv.FormatFloat(cm360, 'g', -1, 64)))
+
+			resultWindow.Show()
+			formWindow.Close()
+		},
+	}
+
+	formWindow.SetContent(form)
+	formWindow.ShowAndRun()
+}
+
+func cmpi(iyaw float64, sensitivity float64, dpi int64) float64 {
 	return (360.0 * 2.54 / (iyaw * sensitivity * float64(dpi)))
 }
 
-func sens(iyaw float64, fyaw float64, sensitivity float64, iDPI int32, fDPI int32) float64 {
+func sens(iyaw float64, fyaw float64, sensitivity float64, iDPI int64, fDPI int64) float64 {
 	return (sensitivity * iyaw * float64(iDPI) / (fyaw * float64(fDPI)))
 }
 
